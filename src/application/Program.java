8b93dba76cc2eb8fbb754d50e3d3761dd3731fab
Wellington Andrade <wmazoni@protonmail.com>
2020-03-31 05:28:12 -0300

    SellerDAOJDBC and DAOFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DAOFactory;
+import model.dao.SellerDAO;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -12,6 +14,9 @@ public class Program {
 		Department obj = new Department(1, "Books");
 		
 		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+		
+		SellerDAO sellerDAO = DAOFactory.createSellerDAO();
+		
 		System.out.println(seller);
 	}
 
